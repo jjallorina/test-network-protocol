cb428d9543a331e619937395905ac803ae547c31
jjallorina <jjallorina@apsiwifi.com>
2019-09-03 20:26:59 +0800

    added server code from beejguide

---
+++ beejguide/server.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+/*
+struct addrinfo
+{
+  int ai_flags;                 // Input flags.  
+  int ai_family;                // Protocol family for socket.  
+  int ai_socktype;              // Socket type.  
+  int ai_protocol;              // Protocol for socket.  
+  socklen_t ai_addrlen;         // Length of socket address.  
+  struct sockaddr *ai_addr;     // Socket address for socket.  
+  char *ai_canonname;           // Canonical name for service location.  
+  struct addrinfo *ai_next;     //  Pointer to next in list. 
+};
+
+struct sockaddr_storage
+{
+    __SOCKADDR_COMMON (ss_);     // Address family, etc.  
+    char __ss_padding[_SS_PADSIZE];
+    __ss_aligntype __ss_align;   // Force desired alignment.  
+};
+*/
+
+#define USEWEB
+#if defined(USEWEB)
+#define PORT "8080"
+#else
+#define PORT "4000"
+#endif
+
+#define BACKLOG 10
+
+void sigchld_handler(int s)
+{
+    //waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+    
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+
+}
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if(sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+int main(int argc, char **argv)
+{
+    int status = 0;
+    int sockfd = 0;
+    int newfd = 0;
+    struct addrinfo hints, *servinfo = NULL, *p = NULL;
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes = 1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+    
+    
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    //hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if((rv = getaddrinfo( NULL, PORT, &hints, &servinfo) != 0))
+    {
+        fprintf(stderr, "getaddrinfo: [%s]\n", gai_strerror(status));
+        return 1;
+    }
+
+    printf("preparing to call socket and bind syscall . . .\n");
+    printf("bind using port [%s] \n", PORT);
+    for( p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype,
+        servinfo->ai_protocol)) == -1 ) {
+            perror("server: socket");
+            continue;
+        }
+        else
+            printf("successfuly executed socket syscall with sockfd[%d]\n", sockfd);
+
+        if((setsockopt( sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, 
+            sizeof(int))) == -1 ) {  
+            perror("server: setsockopt");
+            exit(1);
+        }
+        else
+            printf("successfuly executed setsockopt syscall\n");
+
+
+        if( bind( sockfd, p->ai_addr, p->ai_addrlen) == -1 ) {
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+    
+    printf("freeing servinfo . .\n");
+    freeaddrinfo(servinfo);
+
+    if(p == NULL){
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if(listen(sockfd, BACKLOG) == -1){
+        perror("listen");
+        exit(1);
+    }
+
+    //setup signal handler
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if(sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
+        perror("sigaction:");
+        exit(1);
+    }
+
+    printf("server running: waiting for connections . . .\n");
+    
+    const char message[] = "This message sent to you . . .";
+    while(1){
+        sin_size = sizeof(their_addr);
+        if( (newfd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size))
+        == -1 )
+        {
+            perror("accept:");
+            continue;
+        } 
+
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr
+        *)&their_addr), s, sizeof(s)); 
+        printf("server running: got connection from [%s]\n", s);
+    
+        if( send(newfd, message, sizeof(message), 0) == -1)
+        {
+            perror("send:");
+            close(newfd);
+            close(sockfd);
+            exit(1);
+        }
+    }
+    close(newfd); 
+    close(sockfd);
+    return 0;
+}
